7f0374bd69ec86a253e68be8eaa94637f1bd4531
nagitoru <yokuha188ku@gmail.com>
2020-09-18 15:59:21 +0900

    Allergyテーブルの作成と初期値の設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Allergy.first_or_create([
+{ id: 1, name: '卵'},
+{ id: 2, name: '乳'},
+{ id: 3, name: '小麦'},
+{ id: 4, name: 'えび'},
+{ id: 5, name: 'かに'},
+{ id: 6, name: '落花生'},
+{ id: 7, name: 'そば'},
+{ id: 8, name: 'アーモンド'},
+{ id: 9, name: 'あわび'},
+{ id: 10, name: 'いか'},
+{ id: 11, name: 'いくら'},
+{ id: 12, name: 'オレンジ'},
+{ id: 13, name: 'カシューナッツ'},
+{ id: 14, name: 'キウイフルーツ'},
+{ id: 15, name: '牛肉'},
+{ id: 16, name: 'くるみ'},
+{ id: 17, name: 'ごま'},
+{ id: 18, name: '鮭'},
+{ id: 19, name: 'サバ'},
+{ id: 20, name: '大豆'},
+{ id: 21, name: '鶏肉'},
+{ id: 22, name: 'バナナ'},
+{ id: 23, name: '豚肉'},
+{ id: 24, name: '松茸'},
+{ id: 25, name: 'もも'},
+{ id: 26, name: 'やまいも'},
+{ id: 27, name: 'りんご'},
+{ id: 28, name: 'ゼラチン'},
+{ id: 29, name: 'その他'},
+])
