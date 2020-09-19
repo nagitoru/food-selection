e647663fcfe6f4ad344ed621619e68d992b4fdf4
nagitoru <yokuha188ku@gmail.com>
2020-09-19 13:48:02 +0900

    バリデーションとエラーメッセージ

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,11 @@ class Item < ApplicationRecord
   has_one_attached :image
   has_many :item_allergies
   has_many :allergies, through: :item_allergies
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :text
+    validates :allergy_ids
+  end
 end
