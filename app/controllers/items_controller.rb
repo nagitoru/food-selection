6fffa1a983a7912b883444bfdb03c28e7ec43eb2
nagitoru <yokuha188ku@gmail.com>
2020-09-20 17:30:02 +0900

    商品情報更新機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :destroy, :edit]
+  before_action :set_item, only: [:show, :destroy, :edit, :update]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -32,6 +32,14 @@ class ItemsController < ApplicationController
 
   def edit
     redirect_to root_path unless current_user == @item.user
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
   end
 
   private
